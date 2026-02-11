2aa4d9434774df79cb993dae7d21f033c323acb1
Shanice <30316866+swilldd@users.noreply.github.com>
2026-02-11 09:23:51 +0000

    Add input form for making predictions

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,6 @@ import streamlit as st
 
 # url = 'http://localhost:9696/predict'
 
-patient_history = {
-    'older_maternal_age': 'yes',
-    'high_pre_pregnancy_bmi_or_overweight': 'yes',
-    'family_history_of_diabetes': 'yes',
-    'socioeconomic_factors_deprivation_quintile': '2', 
-    'presence_of_t2dm_associated_gene_variants': 'yes',
-    'ethnicity': 'Asian',
-    'multiparity': 'yes',
-    'insulin_treatment_during_pregnancy': 'yes',
-    'pregnancy_complications_hypertensive_disorders': 'no',
-    'pregnancy_complications_preterm_delivery': 'no',
-    'pregnancy_complications_pph': 'no',
-    'gestational_weight_gain': 'yes',
-    'abnormal_ogtt_results': 'Abnormal',
-    'elevated_hba1c_during_pregnancy': 'Normal',
-    'large_for_gestational_age': 'no',
-    'macrosomia_baby_birth_weightdelivered_a_baby_greater_than_3_5kg': 'yes',
-    'instrumental_delivery': 'no',
-    'nicu_admission': 'no',
-    'obesity_or_unhealthy_postpartum_weight_gain': 'yes',
-    'physical_inactivity': 'yes',
-    'unhealthy_diet': 'yes',
-    'smoking': 'yes',
-    'alcohol_intake': 'no',
-    'does_not_undergo_postpartum_glucose_screening': 'no',
-    'breastfeeding': 'no',
-    'history_of_recurrence_of_gdm': 'yes',
-}
-
 # response = requests.post(url, json=patient_history)
 
 # t2dm_risk_prediction = response.json()
@@ -48,36 +19,52 @@ patient_history = {
 
 
 
-# TODO: Create function for input form for predictions
+# TODO [X]: Create function for input form for predictions
 
 def patient_history_form():
     """
     Function to generate form for collecting patient history to make predictions.
 
-    Returns st.Form
+    Returns st.form container
     """
     with st.form("prediction-input"):
         st.write("Please enter your patient history below:")
-        older_maternal_age = st.radio("Did the patient experience a geriatric pregnancy?", ["Yes", "No"], horizontal=True).lower()
-        high_pre_pregnancy_bmi_or_overweight = st.slider("Pre-pregnancy BMI")
+        older_maternal_age = st.radio("Was the patient of advanced maternal age (≥35 years at delivery)?", ["Yes", "No"], horizontal=True).lower()
+        prepregnancy_bmi = st.slider("What was the patients pre-pregnancy BMI?")
+        if prepregnancy_bmi >= 25:
+            high_pre_pregnancy_bmi_or_overweight = "yes"
+        else:
+            high_pre_pregnancy_bmi_or_overweight = "no"
         family_history_of_diabetes = st.radio("Does the patient have a family history of diabetes?", ["Yes", "No"], horizontal=True).lower()
         socioeconomic_factors_deprivation_quintile = st.selectbox("Which socioeconomic group does the patient fall into?", [1, 2, 3, 4, 5])
         presence_of_t2dm_associated_gene_variants = st.radio("Does the patient have any genes associated with type II diabetes?", ["Yes", "No"], horizontal=True).lower()
         ethnicity = st.selectbox("What is the ethnicity of your patient?", ["White", "Black", "Asian", "Mixed", "Other"])
-        multiparity = st.radio("Multiparity?", ["Yes", "No"], horizontal=True).lower()
+        multiparity = st.radio("Does your patient have a history of multiparity (≥2 prior deliveries)?", ["Yes", "No"], horizontal=True).lower()
         insulin_treatment_during_pregnancy = st.radio("Did your patient have to receive an insulin treatment during their pregrnancy?", ["Yes", "No"], horizontal=True).lower()
         pregnancy_complications_hypertensive_disorders = st.radio("Did your patient suffer any hypertensive disorder complications during their pregnancy?", ["Yes", "No"], horizontal=True).lower()
-        pregnancy_complications_preterm_delivery = st.radio("pregnancy_complications_preterm_delivery?", ["Yes", "No"], horizontal=True).lower()
-        # multiparity = st.radio("Multiparity?", ["Yes", "No"], horizontal=True).lower()
-        # multiparity = st.radio("Multiparity?", ["Yes", "No"], horizontal=True).lower()
-        # multiparity = st.radio("Multiparity?", ["Yes", "No"], horizontal=True).lower()
-        # multiparity = st.radio("Multiparity?", ["Yes", "No"], horizontal=True).lower()
-        # multiparity = st.radio("Multiparity?", ["Yes", "No"], horizontal=True).lower()
-        # TODO: Add rest of questions required to complete JSON request to be sent to API endpoint
+        pregnancy_complications_preterm_delivery = st.radio("Did your patient have a preterm delivery?", ["Yes", "No"], horizontal=True).lower()
+        pregnancy_complications_pph = st.radio("Did your patient suffer any PPH complications during birth?", ["Yes", "No"], horizontal=True).lower()
+        gestational_weight_gain = st.radio("Did the patient have excessive gestational weight gain?", ["Yes", "No"], horizontal=True).lower()
+        abnormal_ogtt_results = st.radio("What were the results of the patients ogtt?", ["Normal", "Abnormal"], horizontal=True)
+        elevated_hba1c_during_pregnancy = st.radio("Did your patient have elevated hba1c during pregnancy?", ["Normal", "Abnormal"], horizontal=True)
+        large_for_gestational_age = st.radio("Was the baby considered large for gestational age?", ["Yes", "No"], horizontal=True).lower()
+        macrosomia_baby_birth_weightdelivered_a_baby_greater_than_3_5kg = st.radio("Did the newborn have macrosomia? (weight at birth above 3.5kg)", ["Yes", "No"], horizontal=True).lower()
+        instrumental_delivery= st.radio("Were instruments required during birth?", ["Yes", "No"], horizontal=True).lower()
+        nicu_admission = st.radio("Was the newborn admitted to the nicu?", ["Yes", "No"], horizontal=True).lower()
+        obesity_or_unhealthy_postpartum_weight_gain = st.radio("Did your patient suffer with unhealthy weight gain post partum?", ["Yes", "No"], horizontal=True).lower()
+        physical_inactivity = st.radio("Is your patient physically inactive?", ["Yes", "No"], horizontal=True).lower()
+        unhealthy_diet = st.radio("Is your patient's diet considered unhealthy?", ["Yes", "No"], horizontal=True).lower()
+        smoking = st.radio("Did your patient smoke during or after pregnancy?", ["Yes", "No"], horizontal=True).lower()
+        alcohol_intake = st.radio("Did your patient drink alcohol during or after pregnancy?", ["Yes", "No"], horizontal=True).lower()
+        does_not_undergo_postpartum_glucose_screening = st.radio("Has the patient failed to complete recommended postpartum glucose screening?", ["Yes", "No"], horizontal=True).lower()
+        breastfeeding = st.radio("Does your patient breastfeed?", ["Yes", "No"], horizontal=True).lower()
+        history_of_recurrence_of_gdm = st.radio("Does your patient have a history of recurring gestational diabetes?", ["Yes", "No"], horizontal=True).lower()
+
+        # TODO [X]: Add rest of questions required to complete JSON request to be sent to API endpoint 
         # Every form must have a submit button.
         submitted = st.form_submit_button("Submit")
         if submitted:
-            # TODO: Add a way to convert responses into JSON request that can be sent to api endpoint.
+            # TODO [X]: Add a way to convert responses into JSON request that can be sent to api endpoint.
             patient_history = {
                 'older_maternal_age': older_maternal_age,
                 'high_pre_pregnancy_bmi_or_overweight': high_pre_pregnancy_bmi_or_overweight,
@@ -89,25 +76,25 @@ def patient_history_form():
                 'insulin_treatment_during_pregnancy': insulin_treatment_during_pregnancy,
                 'pregnancy_complications_hypertensive_disorders': pregnancy_complications_hypertensive_disorders,
                 'pregnancy_complications_preterm_delivery': pregnancy_complications_preterm_delivery,
-                'pregnancy_complications_pph': 'no',
-                'gestational_weight_gain': 'yes',
-                'abnormal_ogtt_results': 'Abnormal',
-                'elevated_hba1c_during_pregnancy': 'Normal',
-                'large_for_gestational_age': 'no',
-                'macrosomia_baby_birth_weightdelivered_a_baby_greater_than_3_5kg': 'yes',
-                'instrumental_delivery': 'no',
-                'nicu_admission': 'no',
-                'obesity_or_unhealthy_postpartum_weight_gain': 'yes',
-                'physical_inactivity': 'yes',
-                'unhealthy_diet': 'yes',
-                'smoking': 'yes',
-                'alcohol_intake': 'no',
-                'does_not_undergo_postpartum_glucose_screening': 'no',
-                'breastfeeding': 'no',
-                'history_of_recurrence_of_gdm': 'yes',
+                'pregnancy_complications_pph': pregnancy_complications_pph,
+                'gestational_weight_gain': gestational_weight_gain,
+                'abnormal_ogtt_results': abnormal_ogtt_results,
+                'elevated_hba1c_during_pregnancy': elevated_hba1c_during_pregnancy,
+                'large_for_gestational_age': large_for_gestational_age,
+                'macrosomia_baby_birth_weightdelivered_a_baby_greater_than_3_5kg': macrosomia_baby_birth_weightdelivered_a_baby_greater_than_3_5kg,
+                'instrumental_delivery': instrumental_delivery,
+                'nicu_admission': nicu_admission,
+                'obesity_or_unhealthy_postpartum_weight_gain': obesity_or_unhealthy_postpartum_weight_gain,
+                'physical_inactivity': physical_inactivity,
+                'unhealthy_diet': unhealthy_diet,
+                'smoking': smoking,
+                'alcohol_intake': alcohol_intake,
+                'does_not_undergo_postpartum_glucose_screening': does_not_undergo_postpartum_glucose_screening,
+                'breastfeeding': breastfeeding,
+                'history_of_recurrence_of_gdm': history_of_recurrence_of_gdm,
             }
 
-            st.write(patient_history)
+            return patient_history
 
 
 
@@ -124,12 +111,14 @@ def main():
     with tab_1:
         st.header("Make New Predictions", text_alignment="center")
 
-        patient_history_form()
+        history = patient_history_form()
 
         with st.container():
             st.header("Explaining the results", text_alignment="center")
             st.write("This section is for explaining why the model made the prediction that it did.")
             st.write("There should be a graph or visual to appear to help explain")
+            if history:
+                st.write(history)
 
 
     with tab_2:
